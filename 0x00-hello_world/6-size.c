5b51cf0dacb8c3a01d5ac6f13bc7d80f62c34f02
alice wangare <wangarealicia72@gmail.com>
2023-06-28 10:27:26 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - A program that prints the size of various computer types
+ * Return 0 (Success)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+printf("size of the char: %lu byte(s)/n", (unsigned long)size of (a));
+printf("size of the int: %lu byte(s)/n", (unsigned long)size of (b));
+printf("size of the long int: %lu byte(s)/n", (unsigned long)size of (c));
+printf("size of the long long int: %lu byte(s)/n", (unsigned long)size of (d));
+printf("size of the float: %lu byte(s)/n", (unsigned long)size of (f));
+return (0);
+}
